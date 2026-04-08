b47e29b1d3b79953f54ec97e3e0c779a1b3ab092
fixeg683 <jacobotana96@gmail.com>
2026-04-08 12:26:59 +0300

    modified api/views.py and payments/urls.py

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from api.views import initiate_payment, mpesa_callback
+from . import views
 
 urlpatterns = [
-    path('initiate/', initiate_payment, name='mpesa-initiate'),
-    path('callback/', mpesa_callback, name='mpesa-callback'),
+    path('pay/', views.initiate_payment, name='initiate_payment'),
+    path('payments/callback/', views.mpesa_callback, name='mpesa_callback'),
+    path('verify-payment/', views.verify_payment, name='verify_payment'),
+    path('my-paid-products/', views.my_downloads, name='my_downloads'),
 ]
