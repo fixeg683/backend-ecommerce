54a1db173d2a6ea16d63b7d28ec44e7335cc16e6
fixeg683 <jacobotana96@gmail.com>
2026-04-08 14:03:49 +0300

    modified api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -9,18 +9,15 @@ router.register(r'categories', CategoryViewSet, basename='categories')
 
 urlpatterns = [
     path('', include(router.urls)),
-
     # AUTH
     path('register/', views.register_user),
     path('users/me/', views.current_user),
-
     # PAYMENT
     path('pay/', views.initiate_payment),
     path('payments/callback/', views.mpesa_callback),
-
+    path('verify-payment/', views.verify_payment),  # ← ADD THIS
     # ORDERS
     path('orders/', views.my_orders),
-
     # DOWNLOADS
     path('my-downloads/', views.my_downloads),
     path('my-paid-products/', views.my_downloads),
