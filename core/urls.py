accf296556abfff9160f26f8779139179f813c0a
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2026-04-18 22:07:46 +0300

    Clean up urls.py and add importance note
    Removed commented lines and added a note for importance.

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # This connects your api app. 
-    # Any URL in api/urls.py will now start with /api/
+
+    # ✅ IMPORTANT
     path('api/', include('api.urls')),
 ]
-
-# Serving static and media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
