b063fd9eb9ad5d85132ca2cc36208dc91075ec70
fixeg683 <jacobotana96@gmail.com>
2026-05-10 15:53:36 +0300

    resolving payment errors

--- core/urls.py
+++ core/urls.py
@@ -1,32 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-# ✅ ROOT VIEW
-@api_view(['GET'])
-def home(request):
-    return Response({
-        "message": "E-Commerce API is running 🚀",
-        "endpoints": {
-            "api": "/api/",
-            "admin": "/admin/",
-            "login": "/api/token/",
-        }
-    })
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # ✅ FIX HERE
-    path('', home),
-
     path('admin/', admin.site.urls),
+    # This connects your api app. 
+    # Any URL in api/urls.py will now start with /api/
     path('api/', include('api.urls')),
-
-    # JWT
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
+
+# Serving static and media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
