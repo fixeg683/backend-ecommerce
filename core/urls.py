e5267656c2e41711a0e5e54c0ad9d4435e87b444
fixeg683 <jacobotana96@gmail.com>
2026-05-23 00:29:15 +0300

    modified core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok", "message": "Nexusmall API is running."})
+
 
 urlpatterns = [
+    path('', health_check),
     path('admin/', admin.site.urls),
-
-    # API
     path('api/', include('api.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
