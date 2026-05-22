514aeee4f2a97603942a9ae81fedd7518156e08d
fixeg683 <jacobotana96@gmail.com>
2026-05-22 14:30:34 +0300

    modified core/urls

--- core/urls.py
+++ core/urls.py
@@ -1,35 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-
-@api_view(['GET'])
-@authentication_classes([])
-@permission_classes([AllowAny])
-def home(request):
-    return Response({
-        "message": "E-Commerce API is running 🚀",
-        "endpoints": {
-            "api": "/api/",
-            "admin": "/admin/",
-            "login": "/api/token/",
-            "refresh": "/api/token/refresh/",
-            "register": "/api/register/",
-            "products": "/api/products/",
-        }
-    })
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),          # ← includes api/urls.py ONLY, never core/urls.py
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # API
+    path('api/', include('api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
