370a1b13737a4b7b7f381a8b78f3e22c079f0a07
fixeg683 <jacobotana96@gmail.com>
2026-05-26 00:43:46 +0300

    modified core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,35 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+
+def home(request):
+    return JsonResponse({
+        "message": "Backend API is running successfully"
+    })
+
+
 urlpatterns = [
+    path('', home),
+
     path('admin/', admin.site.urls),
 
-    # JWT AUTH
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(
+        'api/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
 
-    # App routes
+    path(
+        'api/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+
     path('api/', include('api.urls')),
 ]
