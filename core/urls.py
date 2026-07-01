d24c519aafd8e30faed6cec0f35e9b2e859afa7e
fixeg683 <jacobotana96@gmail.com>
2026-07-01 06:43:17 +0300

    file updates

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,7 @@ def home(request):
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/chatbot/', include('chatbot.urls')),
     path('api/chatbot/webhooks/', include('chatbot.webhook_urls')),
+    path('api/chatbot/', include('chatbot.urls')),
+    path('api/', include('api.urls')),
 ]
