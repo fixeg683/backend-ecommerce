bf2c350ac2db9d5620f5fde35f87d886c6fd419d
fixeg683 <jacobotana96@gmail.com>
2026-09-24 19:27:29 +0300

    fix(settings): restore django.contrib.messages to installed apps and middleware

--- core/settings.py
+++ core/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'corsheaders',
